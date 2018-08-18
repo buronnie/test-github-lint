d69423fa777ae002d47e424b9a9c9f80056d8d9e
kai.guo <kai.guo@servicenow.com>
2018-08-18 08:55:35 -0700

    delete some more code

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const changedLineNumbers = [1, 2, 3].forEach(line => {
 		lineNumberInDiff += 1;
 		return;
 	}
-
 	if (line.startsWith('@@')) {
 		lineNumberInOriginalFile = Number(line.match(/\+([0-9]+)/)[1]) - 1;
 		firstAtSymbol = false;
 		return;
 	}
-	lineNumberInOriginalFile += 1;
 
 	if (line.startsWith('+')) {
 		lineNumbers[lineNumberInOriginalFile] = lineNumberInDiff;
