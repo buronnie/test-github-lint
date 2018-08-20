bc29fecc1472965dfbe35e9df6ae53af7c228f03
kai.guo <kai.guo@servicenow.com>
2018-08-19 18:29:53 -0700

    new change

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
 	"env": {
-			"browser": true,
-			"commonjs": true,
-			"es6": true
+		"browser": true,
+		"commonjs": true,
+		"es6": true
 	},
-	"extends": "airbnb"
+	"extends": "airbnb",
+	"rules": {
+		"no-tabs": 0,
+		"indent": [2, "tab"]
+	}
 };
